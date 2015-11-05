5617b39d00fa333830bd9d91ba52f61a021fa36f
FV <casista@hotmail.com>
2015-11-05 18:11:59 +0100

    Moved

---
+++ GA_Loader.R
@@ -0,0 +1,62 @@
+library(RGoogleAnalytics)
+library(lubridate)
+library(RForcecom)
+
+#############################################################################################
+
+# REM: OVERWRITES values !! (should do only empty fields, instead) ##########################
+
+#############################################################################################
+
+#oauth_token <- Auth(client.id = "387545337049-qoon4gdvbqr2fe9l4cti4qs19sptat2p.apps.googleusercontent.com",client.secret = "xnPrepkF2vDBEcWenP56jiox")
+# Save the token object for future sessions
+#save(oauth_token, file="oauth_token")
+
+# Token -------------------------------------------------------------------
+
+
+# Load the token object
+load("../oauth_token")
+
+profiles <- GetProfiles(oauth_token)
+
+
+# Paris -------------------------------------------------------------------------------------------------------------
+query.list <- Init(start.date = "2014-01-01",
+                   end.date = "2018-11-28",
+                   dimensions = "ga:date,ga:source, ga:medium,ga:campaign",
+                   #metrics = "ga:goal16Completions",
+                   metrics = "ga:sessions",
+                   max.results = 90000,
+                   table.id = "ga:46856038")
+
+# Create the query object
+ga.query <- QueryBuilder(query.list)
+
+GA.paris <- GetReportData(ga.query, oauth_token,paginate_query = F)
+
+# Heidelberg ---------------------------------------------------------------------------------------------------------
+query.list <- Init(start.date = "2014-01-01",
+                   end.date = "2018-11-28",
+                   dimensions = "ga:date,ga:source, ga:medium,ga:campaign",
+                   metrics = "ga:sessions",
+                   max.results = 90000,
+                   table.id = "ga:52041505")
+
+# Create the query object
+ga.query <- QueryBuilder(query.list)
+
+GA.heidelberg <- GetReportData(ga.query, oauth_token,paginate_query = T)
+
+# Madrid --------------------------------------------------------------------------------------------------------------
+query.list <- Init(start.date = "2014-01-01",
+                   end.date = "2018-11-28",
+                   dimensions = "ga:date,ga:source, ga:medium,ga:campaign",
+                   metrics = "ga:sessions",
+                   max.results = 90000,
+                   table.id = "ga:51262284")
+
+# Create the query object
+ga.query <- QueryBuilder(query.list)
+
+GA.madrid <- GetReportData(ga.query, oauth_token,paginate_query = F)
